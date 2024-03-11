b65c556f775b5ccbeaed0c6b29372eedec9406b8
Caron Jang <freekind@naver.com>
2024-03-11 14:51:35 +0900

    함수연습2

--- DemoFunction.py
+++ DemoFunction.py
@@ -7,3 +7,22 @@ def setValue(newValue):
 
 # 호출
 print(setValue(5))
+
+def times(a=10, b=20):
+    return a*b
+
+print(times())
+print(times(5))
+print(times(b=10))
+
+
+# 지역변수와 전역변수
+x = 10
+def func(a):
+    return a+x
+print(func(1))
+
+def func2(a):
+    x = 5
+    return a+x
+print(func2(1))
