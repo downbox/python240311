10c1801a4b0278ff2844e0b834e5be6cdb8312cd
Caron Jang <freekind@naver.com>
2024-03-12 10:35:34 +0900

    클래스 연습 완료

--- BankAccount.py
+++ BankAccount.py
@@ -5,16 +5,19 @@ class BankAccount:
     def __init__(self, id, name, balance):
         self.id = id
         self.name = name 
-        self.balance = balance 
+        # 숨기게 되면(1)
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
     def __str__(self):
         return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+            self.name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.withdraw(3000)
+# 세팅이 안된다(2)
+account1.balance = 99999
 print(account1)
--- class1.py
+++ class1.py
@@ -6,8 +6,12 @@ class Person:
     def __init__(self):
         self.name = "defalut name"
     def print(self):
+        #print(self)
         print("My name is {0}".format(self.name))
 
 # 인스턴스 생성
 p1 = Person()
+p2 = Person()
+p1.name = "전우치"
 p1.print()
+p2.print()
